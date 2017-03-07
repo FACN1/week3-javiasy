018d3ad2b31bc594993808d4238a77a98b7d2fd6
Macintosh Helper <noreply@macintoshhelper.com>
2017-03-07 16:36:23 +0200

    Replace bookId with data-bookId

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var ListenerModule = (function(){
         var bookListNode = document.getElementById("bookList");
         bookListNode.childNodes.forEach(function(childNode){
             childNode.addEventListener("click",function(){
-                console.log(childNode.getAttribute("bookId"));
+                console.log(childNode.getAttribute("data-bookId"));
             })
         })
     }(); //only run after parallel functions maybe
