676360c724eb8f44e4496d6bade8b3dac2edec2f
JWLD <jackwldavies@icloud.com>
2017-03-07 17:35:19 +0200

    add buildRequest function

--- request.js
+++ request.js
@@ -1,25 +1,24 @@
 var RequestModule = (function() {
     // make an API request
-    function makeRequest(url, callback) {
+    function makeRequest(url) {
         var xhr = new XMLHttpRequest();
         xhr.onreadystatechange = function() {
             if (xhr.readyState === 4 && xhr.status === 200) {
-                callback(JSON.parse(xhr.responseText));
+                console.log(JSON.parse(xhr.responseText));
             }
         }
         xhr.open('GET', url);
         xhr.send();
     }
 
-    // temporary callback function
-    function logFunction(result) {
-        console.log(result);
+    // build a request for each Game of Thrones book
+    function buildRequest_GOTBooks(bookNumber) {
+        var url = 'http://anapioficeandfire.com/api/books/' + bookNumber;
+        makeRequest(url);
     }
-
-    makeRequest('http://anapioficeandfire.com/api/books/1', logFunction);
 
     // make these accessible outside the module
     return {
-        makeRequest: makeRequest
+        buildRequest_GOTBooks: buildRequest_GOTBooks
     }
 })();
