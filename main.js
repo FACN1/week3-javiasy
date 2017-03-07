bf492719ae4f9dfbf2c01f3aa022e056eac1bea9
Matt Davis <matthewpeterdavis@gmail.com>
2017-03-07 16:23:20 +0200

    add event listeners to each book title

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 var ListenerModule = (function(){
 
+    var bookListNode = document.getElementById("bookList");
+    bookListNode.childNodes.forEach(function(childNode){
+        childNode.addEventListener("click",function(){
+            console.log(childNode.getAttribute("bookId"));
+        })
+    })
+
 })()
