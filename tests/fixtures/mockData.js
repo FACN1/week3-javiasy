59d3b68f91809afcf3db966e6d3fafeff982dc9b
Matt Davis <matthewpeterdavis@gmail.com>
2017-03-08 12:29:57 +0200

    beautified

--- tests/fixtures/mockData.js
+++ tests/fixtures/mockData.js
@@ -4,7 +4,7 @@ var mockData = {
         name: "A Game of Thrones",
         isbn: "978-0553103540",
         authors: [
-        "George R. R. Martin"
+            "George R. R. Martin"
         ],
         numberOfPages: 694,
         publisher: "Bantam Books",
@@ -12,15 +12,15 @@ var mockData = {
         mediaType: "Hardcover",
         released: "1996-08-01T00:00:00",
         povCharacters: [
-        "http://anapioficeandfire.com/api/characters/148",
-        "http://anapioficeandfire.com/api/characters/208",
-        "http://anapioficeandfire.com/api/characters/232",
-        "http://anapioficeandfire.com/api/characters/339",
-        "http://anapioficeandfire.com/api/characters/583",
-        "http://anapioficeandfire.com/api/characters/957",
-        "http://anapioficeandfire.com/api/characters/1052",
-        "http://anapioficeandfire.com/api/characters/1109",
-        "http://anapioficeandfire.com/api/characters/1303"
+            "http://anapioficeandfire.com/api/characters/148",
+            "http://anapioficeandfire.com/api/characters/208",
+            "http://anapioficeandfire.com/api/characters/232",
+            "http://anapioficeandfire.com/api/characters/339",
+            "http://anapioficeandfire.com/api/characters/583",
+            "http://anapioficeandfire.com/api/characters/957",
+            "http://anapioficeandfire.com/api/characters/1052",
+            "http://anapioficeandfire.com/api/characters/1109",
+            "http://anapioficeandfire.com/api/characters/1303"
         ]
     },
     character1: {
@@ -31,137 +31,134 @@ var mockData = {
         born: "In 289 AC, at Winterfell",
         died: "",
         titles: [
-        "Princess"
+            "Princess"
         ],
         aliases: [
-        "Arya Horseface",
-        "Arya Underfoot",
-        "Arry",
-        "Lumpyface",
-        "Lumpyhead",
-        "Stickboy",
-        "Weasel",
-        "Nymeria",
-        "Squan",
-        "Saltb",
-        "Cat of the Canaly",
-        "Bets",
-        "The Blind Girh",
-        "The Ugly Little Girl",
-        "Mercedenl",
-        "Mercye"
+            "Arya Horseface",
+            "Arya Underfoot",
+            "Arry",
+            "Lumpyface",
+            "Lumpyhead",
+            "Stickboy",
+            "Weasel",
+            "Nymeria",
+            "Squan",
+            "Saltb",
+            "Cat of the Canaly",
+            "Bets",
+            "The Blind Girh",
+            "The Ugly Little Girl",
+            "Mercedenl",
+            "Mercye"
         ],
         father: "",
         mother: "",
         spouse: "",
         allegiances: [
-        "http://anapioficeandfire.com/api/houses/362"
+            "http://anapioficeandfire.com/api/houses/362"
         ],
-        books: [ ],
+        books: [],
         povBooks: [
-        "http://anapioficeandfire.com/api/books/1",
-        "http://anapioficeandfire.com/api/books/2",
-        "http://anapioficeandfire.com/api/books/3",
-        "http://anapioficeandfire.com/api/books/5",
-        "http://anapioficeandfire.com/api/books/8"
+            "http://anapioficeandfire.com/api/books/1",
+            "http://anapioficeandfire.com/api/books/2",
+            "http://anapioficeandfire.com/api/books/3",
+            "http://anapioficeandfire.com/api/books/5",
+            "http://anapioficeandfire.com/api/books/8"
         ],
         tvSeries: [
-        "Season 1",
-        "Season 2",
-        "Season 3",
-        "Season 4",
-        "Season 5",
-        "Season 6"
+            "Season 1",
+            "Season 2",
+            "Season 3",
+            "Season 4",
+            "Season 5",
+            "Season 6"
         ],
         playedBy: [
-        "Maisie Williams"
+            "Maisie Williams"
         ]
     },
-    actor1:{
+    actor1: {
         page: 1,
-        results: [
-        {
-        profile_path: "/7PlTqaeqCNctmHf8UEBjChHID98.jpg",
-        adult: false,
-        id: 1181313,
-        known_for: [
-        {
-        poster_path: "/jIhL6mlT7AblhbHJgEoiBIOUVl1.jpg",
-        popularity: 43.43068,
-        id: 1399,
-        overview: "Seven noble families fight for control of the mythical land of Westeros. Friction between the houses leads to full-scale war. All while a very ancient evil awakens in the farthest north. Amidst the war, a neglected military order of misfits, the Night's Watch, is all that stands between the realms of men and icy horrors beyond.",
-        backdrop_path: "/mUkuc2wyV9dHLG0D0Loaw5pO2s8.jpg",
-        vote_average: 8,
-        media_type: "tv",
-        first_air_date: "2011-04-17",
-        origin_country: [
-        "US"
-        ],
-        genre_ids: [
-        10759,
-        18,
-        10765
-        ],
-        original_language: "en",
-        vote_count: 1901,
-        name: "Game of Thrones",
-        original_name: "Game of Thrones"
-        },
-        {
-        poster_path: "/igDhbYQTvact1SbNDbzoeiFBGda.jpg",
-        popularity: 47.014535,
-        id: 57243,
-        overview: "The Doctor looks and seems human. He's handsome, witty, and could be mistaken for just another man in the street. But he is a Time Lord: a 900 year old alien with 2 hearts, part of a gifted civilization who mastered time travel. The Doctor saves planets for a living – more of a hobby actually, and he's very, very good at it. He's saved us from alien menaces and evil from before time began – but just who is he?",
-        backdrop_path: "/cVWsigSx97cTw1QfYFFsCMcR4bp.jpg",
-        vote_average: 7.14,
-        media_type: "tv",
-        first_air_date: "2005-03-26",
-        origin_country: [
-        "GB"
-        ],
-        genre_ids: [
-        10759,
-        18,
-        10765
-        ],
-        original_language: "en",
-        vote_count: 474,
-        name: "Doctor Who",
-        original_name: "Doctor Who"
-        },
-        {
-        poster_path: "/gtkQDOTyIySEyz3Oo06Mys8xFpY.jpg",
-        adult: false,
-        overview: "A chilling real-time thriller featuring a teenager, Casey, battling with an anonymous cyber-stalker.",
-        release_date: "2015-01-15",
-        original_title: "Cyberbully",
-        genre_ids: [
-        18,
-        53
-        ],
-        id: 317144,
-        media_type: "movie",
-        original_language: "en",
-        title: "Cyberbully",
-        backdrop_path: "/sobY9T0G5V3y57nkxYlShOUTcwq.jpg",
-        popularity: 1.648243,
-        vote_count: 86,
-        video: false,
-        vote_average: 6.5
-        }
-        ],
-        name: "Maisie Williams",
-        popularity: 12.683655
-        }
-        ],
+        results: [{
+            profile_path: "/7PlTqaeqCNctmHf8UEBjChHID98.jpg",
+            adult: false,
+            id: 1181313,
+            known_for: [{
+                    poster_path: "/jIhL6mlT7AblhbHJgEoiBIOUVl1.jpg",
+                    popularity: 43.43068,
+                    id: 1399,
+                    overview: "Seven noble families fight for control of the mythical land of Westeros. Friction between the houses leads to full-scale war. All while a very ancient evil awakens in the farthest north. Amidst the war, a neglected military order of misfits, the Night's Watch, is all that stands between the realms of men and icy horrors beyond.",
+                    backdrop_path: "/mUkuc2wyV9dHLG0D0Loaw5pO2s8.jpg",
+                    vote_average: 8,
+                    media_type: "tv",
+                    first_air_date: "2011-04-17",
+                    origin_country: [
+                        "US"
+                    ],
+                    genre_ids: [
+                        10759,
+                        18,
+                        10765
+                    ],
+                    original_language: "en",
+                    vote_count: 1901,
+                    name: "Game of Thrones",
+                    original_name: "Game of Thrones"
+                },
+                {
+                    poster_path: "/igDhbYQTvact1SbNDbzoeiFBGda.jpg",
+                    popularity: 47.014535,
+                    id: 57243,
+                    overview: "The Doctor looks and seems human. He's handsome, witty, and could be mistaken for just another man in the street. But he is a Time Lord: a 900 year old alien with 2 hearts, part of a gifted civilization who mastered time travel. The Doctor saves planets for a living – more of a hobby actually, and he's very, very good at it. He's saved us from alien menaces and evil from before time began – but just who is he?",
+                    backdrop_path: "/cVWsigSx97cTw1QfYFFsCMcR4bp.jpg",
+                    vote_average: 7.14,
+                    media_type: "tv",
+                    first_air_date: "2005-03-26",
+                    origin_country: [
+                        "GB"
+                    ],
+                    genre_ids: [
+                        10759,
+                        18,
+                        10765
+                    ],
+                    original_language: "en",
+                    vote_count: 474,
+                    name: "Doctor Who",
+                    original_name: "Doctor Who"
+                },
+                {
+                    poster_path: "/gtkQDOTyIySEyz3Oo06Mys8xFpY.jpg",
+                    adult: false,
+                    overview: "A chilling real-time thriller featuring a teenager, Casey, battling with an anonymous cyber-stalker.",
+                    release_date: "2015-01-15",
+                    original_title: "Cyberbully",
+                    genre_ids: [
+                        18,
+                        53
+                    ],
+                    id: 317144,
+                    media_type: "movie",
+                    original_language: "en",
+                    title: "Cyberbully",
+                    backdrop_path: "/sobY9T0G5V3y57nkxYlShOUTcwq.jpg",
+                    popularity: 1.648243,
+                    vote_count: 86,
+                    video: false,
+                    vote_average: 6.5
+                }
+            ],
+            name: "Maisie Williams",
+            popularity: 12.683655
+        }],
         total_results: 1,
         total_pages: 1
     },
-    actorId:{
+    actorId: {
         adult: false,
         also_known_as: [
-        "Margaret Constance Williams",
-        "Margaret Williams"
+            "Margaret Constance Williams",
+            "Margaret Williams"
         ],
         biography: "Margaret Constance \"Maisie\" Williams is an English actress. She made her professional acting debut as Arya Stark in the HBO fantasy television series Game of Thrones, for which she won the EWwy Award for Best Supporting Actress in a Drama, the Portal Award for Best Supporting Actress – Television and Best Young Actor, and the Saturn Award for Best Performance by a Younger Actor.",
         birthday: "1997-04-15",
@@ -177,4 +174,4 @@ var mockData = {
     }
 
 
-    }
+}
