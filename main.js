727b9d5910e6c3fde48f53796fed15b59a323019
JWLD <jackwldavies@icloud.com>
2017-03-08 16:46:18 +0200

    remove extract data module

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ var ListenerModule = (function(){
     }
 })();
 
-// helper functions to extract data from API responses
+/*// helper functions to extract data from API responses
 var ExtractDataModule = (function() {
     var gameOfThrones = {
         extractCharacters: function(response){
@@ -64,13 +64,12 @@ var ExtractDataModule = (function() {
         gameOfThrones: gameOfThrones,
         movieDB: movieDB
     }
-})();
+})();*/
 
 var CallbackModule = (function() {
     // extracts char URLs from repsonse, and uses them to make requests for actor names
     var getCharURLs = function(response) {
-        var characterList = ExtractDataModule.gameOfThrones.extractCharacters(response);
-
+        var characterList = response.povCharacters;
         // loop through characters
         characterList.forEach(function(charUrl) {
             RequestModule.makeRequest(charUrl, getActorName);
@@ -79,28 +78,28 @@ var CallbackModule = (function() {
 
     // extracts actor name from response, uses it to build a URL, and makes request for actor id
     var getActorName = function(response) {
-        var actorName = ExtractDataModule.gameOfThrones.extractActors(response);
-        // only continue with defined actor names
-        if (actorName !== undefined) {
-            var url = RequestModule.buildURL_actorId(actorName);
-            RequestModule.makeRequest(url, getActorId);
+        if (response.playedBy[0] === undefined) {
+            return;
         }
+        var actorName = response.playedBy[0];
+        var url = RequestModule.buildURL_actorId(actorName);
+        RequestModule.makeRequest(url, getActorId);
     };
 
     // extracts actor id from response, uses it to build a URL, and makes request for IMDb link
     var getActorId = function(response) {
-        var actorId = ExtractDataModule.movieDB.getActorId(response);
+        var actorId = response.results[0].id;
         var url = RequestModule.buildURL_actorInfo(actorId);
         RequestModule.makeRequest(url, getIMDbLink);
     };
 
     // extracts IMDb link from response, and does render stuff
     var getIMDbLink = function(response) {
-        var link = ExtractDataModule.movieDB.getIMDBLink(response);
-        // only continue with full imdb links
-        if (link !== undefined) {
-            RenderModule.renderIMDbLink(link);
+        if (response.imdb_id === null) {
+            return;
         }
+        var link = 'http://imdb.com/name/' + response.imdb_id;
+        RenderModule.renderIMDbLink(link);
     };
 
     return {
