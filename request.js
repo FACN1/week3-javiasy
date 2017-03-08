9237931c3e1308c30e09a08bae5f745debcb5573
JWLD <jackwldavies@icloud.com>
2017-03-08 10:15:16 +0200

    finished request module

--- request.js
+++ request.js
@@ -11,19 +11,35 @@ var RequestModule = (function() {
         xhr.send();
     }
 
-    // build a request for each Game of Thrones book
+    // build a request for each GoT book - to get a character list
     function buildRequest_GOTBooks(bookNumber) {
         var url = 'http://anapioficeandfire.com/api/books/' + bookNumber;
         makeRequest(url);
     }
 
-    // build a movieDB request for each actor
-    function buildRequest_actorInfo(actorName) {
+    // build a movieDB request for an actor - to get their id
+    function buildRequest_actorId(actorName) {
+        // replace spaces in actorName with %20
+        actorName.replace(/\s/gi, '%20');
+
+        // build URL
         var myKey = config.theMovieDBKey;
+        var url = 'https://api.themoviedb.org/3/search/person?api_key=' + myKey + '&query=' + actorName;
+
+        // make the request
+        makeRequest(url);
+    }
+
+    // build a movieDB request for an actor id - to get more info
+    function buildRequest_actorInfo(actorId) {
+        var url = 'https://api.themoviedb.org/3/person/' + actorId + '?api_key=' + myKey;
+        makeRequest(url);
     }
 
     // make these accessible outside the module
     return {
-        buildRequest_GOTBooks: buildRequest_GOTBooks
+        buildRequest_GOTBooks: buildRequest_GOTBooks, // takes a book number
+        buildRequest_actorId: buildRequest_actorId, // takes an actor name
+        buildRequest_actorInfo: buildRequest_actorInfo // takes an actor id
     }
 })();
