d360663f663f32e5b386c8eb05e858caeffbaf23
JWLD <jackwldavies@icloud.com>
2017-03-08 15:59:44 +0200

    add and implement callback module

--- main.js
+++ main.js
@@ -12,8 +12,13 @@ var ListenerModule = (function(){
     }; // only run after parallel functions maybe
 
     // Initialise eventListener to log id.
-    initBookListener(function(bookNode) {
-        console.log(bookNode.getAttribute("data-bookId"));
+    initBookListener(function(childNode) {
+        var bookNumber = childNode.getAttribute("data-bookId");
+        var url = RequestModule.buildURL_GOTBooks(bookNumber);
+
+        // kick off callback chain from here
+        var callback = CallbackModule.getCharURLs;
+        RequestModule.makeRequest(url, callback);
     });
 
     return {
@@ -21,6 +26,7 @@ var ListenerModule = (function(){
     }
 })();
 
+// helper functions to extract data from API responses
 var ExtractDataModule = (function() {
     var gameOfThrones = {
         extractCharacters: function(response){
@@ -28,7 +34,9 @@ var ExtractDataModule = (function() {
         },
 
         extractActors: function(response){
-            return response.playedBy;
+            if (response.playedBy[0] !== undefined) {
+                return response.playedBy[0];
+            }
         }
     }
 
@@ -40,12 +48,56 @@ var ExtractDataModule = (function() {
 
         // takes actor id output from movieDB request, and returns IMDb link
         getIMDBLink: function(data) {
-            return 'http://imdb.com/name/' + data.imdb_id;
+            if (data.imdb_id !== null) {
+                return 'http://imdb.com/name/' + data.imdb_id;
+            }
         }
     }
 
     return {
         gameOfThrones: gameOfThrones,
         movieDB: movieDB
+    }
+})();
+
+var CallbackModule = (function() {
+    // extracts char URLs from repsonse, and uses them to make requests for actor names
+    var getCharURLs = function(response) {
+        var characterList = ExtractDataModule.gameOfThrones.extractCharacters(response);
+
+        // loop through characters
+        characterList.forEach(function(charUrl) {
+            RequestModule.makeRequest(charUrl, getActorName);
+        });
+    }
+
+    // extracts actor name from response, uses it to build a URL, and makes request for actor id
+    var getActorName = function(response) {
+        var actorName = ExtractDataModule.gameOfThrones.extractActors(response);
+        // only continue with defined actor names
+        if (actorName !== undefined) {
+            var url = RequestModule.buildURL_actorId(actorName);
+            RequestModule.makeRequest(url, getActorId);
+        }
+    };
+
+    // extracts actor id from response, uses it to build a URL, and makes request for IMDb link
+    var getActorId = function(response) {
+        var actorId = ExtractDataModule.movieDB.getActorId(response);
+        var url = RequestModule.buildURL_actorInfo(actorId);
+        RequestModule.makeRequest(url, getIMDbLink);
+    };
+
+    // extracts IMDb link from response, and does render stuff
+    var getIMDbLink = function(response) {
+        var link = ExtractDataModule.movieDB.getIMDBLink(response);
+        // only continue with full imdb links
+        if (link !== undefined) {
+            console.log(link);
+        }
+    };
+
+    return {
+        getCharURLs: getCharURLs
     }
 })();
