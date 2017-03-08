354a394bba3752ad2423847dc543c0db28950115
Macintosh Helper <noreply@macintoshhelper.com>
2017-03-08 11:10:26 +0200

    Add callback to book eventListener

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
 var ListenerModule = (function(){
 
-    var initBookListener = function() {
+    var initBookListener = function(callback) {
         var bookListNode = document.getElementById("bookList");
         bookListNode.childNodes.forEach(function(childNode){
-            childNode.addEventListener("click",function(){
-                console.log(childNode.getAttribute("data-bookId"));
+            childNode.addEventListener("click",function(event){
+                event.preventDefault();
+                callback(childNode);
             });
         });
-    }(); //only run after parallel functions maybe
+    }; //only run after parallel functions maybe
+    initBookListener(function(bookNode) {
+        console.log(bookNode.getAttribute("data-bookId"));
+    })
+    return {
+        initBookListener: initBookListener
+    }
     // initBookListener();
 })();
