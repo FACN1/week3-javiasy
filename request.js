87c2f9e04449fbe8200786e38b6a185b7f12974d
JWLD <jackwldavies@icloud.com>
2017-03-09 12:50:03 +0200

    #51 refactored buildURL functions into a single function

--- request.js
+++ request.js
@@ -11,33 +11,20 @@ var RequestModule = (function() {
         xhr.send();
     }
 
-    // build a request for each GoT book - to get a character list
-    function buildURL_GOTBooks(bookNumber) {
-        var url = 'http://anapioficeandfire.com/api/books/' + bookNumber;
-        return url;
-    }
+    // build url string
+    function buildUrl(type, params) {
+        var urls = {
+            book: 'http://anapioficeandfire.com/api/books/' + params,
+            name: 'https://api.themoviedb.org/3/search/person?api_key=' + config.theMovieDBKey + '&query=' + params,
+            id: 'https://api.themoviedb.org/3/person/' + params + '?api_key=' + config.theMovieDBKey
+        };
 
-    // build a movieDB request for an actor - to get their id
-    function buildURL_actorId(actorName) {
-        // replace spaces in actorName with %20
-        actorName = actorName.replace(/\s/gi, '%20');
-
-        // build and return URL
-        var url = 'https://api.themoviedb.org/3/search/person?api_key=' + config.theMovieDBKey + '&query=' + actorName;
-        return url;
-    }
-
-    // build a movieDB request for an actor id - to get more info
-    function buildURL_actorInfo(actorId) {
-        var url = 'https://api.themoviedb.org/3/person/' + actorId + '?api_key=' + config.theMovieDBKey;
-        return url;
+        return urls[type];
     }
 
     // make these accessible outside the module
     return {
         makeRequest: makeRequest,
-        buildURL_GOTBooks: buildURL_GOTBooks, // takes a book number
-        buildURL_actorId: buildURL_actorId, // takes an actor name
-        buildURL_actorInfo: buildURL_actorInfo // takes an actor id
+        buildUrl: buildUrl
     }
 })();
